eeb6d105febb31ce19a5f677b29a58aaf5538f88
saiyedimtiaj <hridoyemtiaz0@gmail.com>
2024-10-03 14:18:37 +0600

    deploying

--- src/components/ClientComponent/ShowContent.tsx
+++ src/components/ClientComponent/ShowContent.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
-import { EditorState, convertFromRaw } from 'draft-js';
-import draftToHtml from 'draftjs-to-html';
-import { convertToRaw } from 'draft-js';
+// import { EditorState, convertFromRaw } from 'draft-js';
+// import draftToHtml from 'draftjs-to-html';
+// import { convertToRaw } from 'draft-js';
 
 const ShowContent = ({ content }: { content: string }) => {
-    const contentState = convertFromRaw(JSON.parse(content));
-    const editorState = EditorState.createWithContent(contentState);
+    // const contentState = convertFromRaw(JSON.parse(content));
+    // const editorState = EditorState.createWithContent(contentState);
     return (
-        <div dangerouslySetInnerHTML={{ __html: draftToHtml(convertToRaw(editorState.getCurrentContent())) }} />
+        <div>
+            {/* <div dangerouslySetInnerHTML={{ __html: draftToHtml(convertToRaw(editorState.getCurrentContent())) }} /> */}
+        </div>
     )
 }
 
